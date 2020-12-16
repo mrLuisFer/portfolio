3335c7f43bf629aebafe7986fc338606a8798dce
mrLuisFer <lolesuncrak@gmail.com>
2020-12-15 23:32:07 -0600

    Adding node-sass and improving global styles with .css and normalize, and adding two fonts

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,10 @@
 import React from "react"
-import "./App.css"
+import "./App.scss"
 
-function App() {
+function App(): JSX.Element {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Here well be the <code>mrLuisFer Portfolio</code>
-        </p>
-      </header>
+      <p>I think i have an idea to the design</p>
     </div>
   )
 }
