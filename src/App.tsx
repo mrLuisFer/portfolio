50f394dc5c5a37dbe072923be2962f87f3568264
mrLuisFer <lolesuncrak@gmail.com>
2020-12-17 22:24:07 -0600

    Adding animate.css and ending the NavBar with information and a first style and fixing some bugs

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,14 @@ import "./scss/_App.scss"
 import HomePage from "./views/HomePage"
 // components
 import NavBar from "./components/NavBar"
+// Assets
+import wavePattern from "./assets/wave-pattern.svg"
 
 export default function App(): JSX.Element {
   // Components for the view
   return (
     <div className="App">
+      <img className="wave-pattern" src={wavePattern} alt="wave-pattern" />
       <NavBar />
       <HomePage />
     </div>
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,10 +1,11 @@
 import React, { useState } from "react"
 import { Link, BrowserRouter as Router } from "react-router-dom"
 
+// Assets
+import doubleArrowRight from "../assets/double-arrow-right.svg"
+
 export default function NavBar() {
   const [showIcons, setShowIcons] = useState(false)
-
-  const div = document.querySelector(".NavBar__social-media-container-icons")
 
   const handleShowIcons = (): void => {
     setShowIcons(!showIcons)
@@ -14,7 +15,9 @@ export default function NavBar() {
     <Router>
       <div className="NavBar">
         <div className="NavBar__logo">
-          <span className="NavBar__logo-title">mrLuisFer</span>
+          <h1 className="NavBar__logo-title">
+            <Link to="/">mrLuisFer</Link>
+          </h1>
         </div>
         <div className="NavBar__links">
           <Link to="/">Contact me</Link>
@@ -24,19 +27,45 @@ export default function NavBar() {
               className="NavBar__social-media__span"
               onClick={handleShowIcons}
             >
+              <img
+                src={doubleArrowRight}
+                alt="double-arrow-right"
+                className={showIcons ? "" : "NavBar__img-volted"}
+              />
               Social Media
+              {showIcons ? ":" : ""}
             </span>
+
             {showIcons ? (
               <div className="NavBar__social-media-icons">
-                <div className="NavBar__social-media-container-icons">
-                  <a href="/">
+                <div className="NavBar__social-media-icons-anchors animate__animated animate__zoomIn animate__faster">
+                  <a
+                    href="https://www.facebook.com/profile.php?id=100013068189807"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
                     <i className="fab fa-facebook-f"></i>
                   </a>
-                  <a href="/">
+                  <a
+                    href="https://twitter.com/lolesuncrak"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
                     <i className="fab fa-twitter"></i>
                   </a>
-                  <a href="/">
+                  <a
+                    href="https://www.linkedin.com/in/luis-fernando-alvarez/"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
                     <i className="fab fa-linkedin-in"></i>
+                  </a>
+                  <a
+                    href="mailto:lolesuncrak@gmail.com"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    <i className="fas fa-paper-plane"></i>
                   </a>
                 </div>
               </div>
