0da7f83e883dbf5e4254f8fd50b8279fa1f52ce1
mrLuisFer <lolesuncrak@gmail.com>
2020-12-17 18:12:16 -0600

    Adding styles to NavBar component and thinking more of the design

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,13 +1,13 @@
 import React from "react"
-import logo from "../assets/hero-pattern.png"
+import heroPattern from "../assets/hero-pattern.png"
 
 export default function Hero(): JSX.Element {
   return (
     <div className="Hero">
-      <img src={logo} alt="" />
+      <img src={heroPattern} alt="hero-pattern" />
       <div className="Hero__personal">
         <h2 className="Hero__personal-title">
-          Hi!, my name is <span>Luis Alvarez</span>
+          Hi!, I'm <span>Luis Alvarez</span>
           <span>
             <i className="fas fa-grip-lines-vertical"></i>
           </span>
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,7 +1,15 @@
-import React from "react"
+import React, { useState } from "react"
 import { Link, BrowserRouter as Router } from "react-router-dom"
 
-export default function NavBar(): JSX.Element {
+export default function NavBar() {
+  const [showIcons, setShowIcons] = useState(false)
+
+  const div = document.querySelector(".NavBar__social-media-container-icons")
+
+  const handleShowIcons = (): void => {
+    setShowIcons(!showIcons)
+  }
+
   return (
     <Router>
       <div className="NavBar">
@@ -11,6 +19,31 @@ export default function NavBar(): JSX.Element {
         <div className="NavBar__links">
           <Link to="/">Contact me</Link>
           <Link to="/">About me</Link>
+          <div className="NavBar__social-media">
+            <span
+              className="NavBar__social-media__span"
+              onClick={handleShowIcons}
+            >
+              Social Media
+            </span>
+            {showIcons ? (
+              <div className="NavBar__social-media-icons">
+                <div className="NavBar__social-media-container-icons">
+                  <a href="/">
+                    <i className="fab fa-facebook-f"></i>
+                  </a>
+                  <a href="/">
+                    <i className="fab fa-twitter"></i>
+                  </a>
+                  <a href="/">
+                    <i className="fab fa-linkedin-in"></i>
+                  </a>
+                </div>
+              </div>
+            ) : (
+              ""
+            )}
+          </div>
         </div>
       </div>
     </Router>
