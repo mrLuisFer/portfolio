cab3110fad82c395cdd0d5dcb632ef2e19665d8a
mrLuisFer <lolesuncrak@gmail.com>
2020-12-25 01:34:59 -0600

    Adding the layout idea to Service component

--- src/components/Services.tsx
+++ src/components/Services.tsx
@@ -7,6 +7,22 @@ export default function Services(): JSX.Element {
         <i className="fas fa-angle-double-right"></i>
         My <span>Services:</span>
       </h2>
+      <div className="Services__container">
+        <div>
+          {/* Icon */}
+          {/* Title */}
+          {/* Text */}
+        </div>
+      </div>
+
+      {/* Cards of 'Specializing in' */}
+
+      <div>
+        <div>
+          {/* Title */}
+          {/* Text */}
+        </div>
+      </div>
     </div>
   )
 }
