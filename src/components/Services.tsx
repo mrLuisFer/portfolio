94ec5893a12711ba14ff6eab23e99ddaf9916a8d
mrLuisFer <lolesuncrak@gmail.com>
2020-12-25 00:55:27 -0600

    refactoring styles in sass files and building the Services component

--- src/components/Services.tsx
+++ src/components/Services.tsx
@@ -2,8 +2,11 @@ import React from "react"
 
 export default function Services(): JSX.Element {
   return (
-    <div>
-      <p>My services</p>
+    <div className="Services">
+      <h2 className="Services__title">
+        <i className="fas fa-angle-double-right"></i>
+        My <span>Services:</span>
+      </h2>
     </div>
   )
 }
