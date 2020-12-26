d2be8602a25207d06aac34e9329ea37d194eceb8
mrLuisFer <lolesuncrak@gmail.com>
2020-12-26 17:46:07 -0600

    Adding some styles and fixing some bugs in Services component

--- src/components/ServicesHtml.tsx
+++ src/components/ServicesHtml.tsx
@@ -3,21 +3,28 @@ import React, { useState } from "react"
 type TServicesHtml = {
   iconClass: string
   title: string
-  description: string
+  description?: string
+  linkReference?: string
 }
 
 export default function ServicesHtml({
   iconClass,
   title,
-  description,
+  description = "",
+  linkReference = "",
 }: TServicesHtml) {
   const [showText, setShowText] = useState<Boolean>(false)
+  const [showLinkReference, setShowLinkReference] = useState<Boolean>(true)
 
   const handleShowText = (): void => {
     // checks if the text exists so as not to break the style of the box
     const hasDescription: number = 40
     if (description.length >= hasDescription) {
       setShowText(!showText)
+    }
+
+    if (linkReference.length <= 1 || linkReference === undefined) {
+      setShowLinkReference(false)
     }
   }
 
@@ -26,10 +33,9 @@ export default function ServicesHtml({
       {/* Card */}
       <div className="Services__container-card">
         {/* Icon (ClassName)*/}
-        <i
-          id="card-icon"
-          className={iconClass + " Services__container-card-icon"}
-        ></i>
+        <span className="Services__container-card-icon">
+          <i id="card-icon" className={iconClass}></i>
+        </span>
         <div>
           {/* Title */}
           <h1 onClick={handleShowText}>
@@ -41,15 +47,24 @@ export default function ServicesHtml({
             {title}
           </h1>
           {/* Text (Description)*/}
-          <p
+          <div
             className={
               showText
                 ? "Services__showed animate__animated animate__fadeIn"
                 : "Services__toShow"
             }
           >
-            {description}
-          </p>
+            <p>{description}</p>
+
+            {showLinkReference ? (
+              <a href={linkReference} target="_blank" rel="noreferrer">
+                <i className="fas fa-link"></i>
+                <span>What is this?</span>
+              </a>
+            ) : (
+              ""
+            )}
+          </div>
         </div>
       </div>
     </>
