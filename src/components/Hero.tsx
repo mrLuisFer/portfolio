c83b2a4fe93625360148c5d01f08f98ae45624a4
mrLuisFer <lolesuncrak@gmail.com>
2020-12-26 10:55:32 -0600

    Finishing the layout in the Services component

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,5 +1,5 @@
 import React from "react"
-import { BrowserRouter as Router } from "react-router-dom"
+import {BrowserRouter as Router} from "react-router-dom"
 // Assets
 import heroIllustration from "../assets/hero-illustration.svg"
 
--- src/components/Services.tsx
+++ src/components/Services.tsx
@@ -1,6 +1,12 @@
-import React from "react"
+import React, { useState } from "react"
 
 export default function Services(): JSX.Element {
+  const [showText, setShowText] = useState<Boolean>(false)
+
+  const handleShowText = () => {
+    setShowText(!showText)
+  }
+
   return (
     <div className="Services">
       <h2 className="Services__title">
@@ -8,10 +14,33 @@ export default function Services(): JSX.Element {
         My <span>Services:</span>
       </h2>
       <div className="Services__container">
-        <div>
+        {/* Card */}
+        <div className="Services__container-card">
           {/* Icon */}
-          {/* Title */}
-          {/* Text */}
+          <i className="fas fa-laptop-code Services__container-card-icon"></i>
+          <div>
+            {/* Title */}
+            <h1 onClick={handleShowText}>
+              <span
+                className={showText ? "Services__toVolted" : "Services__toVolt"}
+              >
+                <i className="fas fa-caret-right"></i>
+              </span>
+              Web Development
+            </h1>
+            {/* Text */}
+            <p
+              className={
+                showText
+                  ? "Services__showed animate__animated animate__fadeIn"
+                  : "Services__toShow"
+              }
+            >
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Neque,
+              eaque! Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              voluptate facere doloribus adipisci, illum qui saepe.
+            </p>
+          </div>
         </div>
       </div>
 
