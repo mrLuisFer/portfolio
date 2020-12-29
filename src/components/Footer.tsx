0ad55937d18ef2bf590a7c3652325282c0e7891c
mrLuisFer <lolesuncrak@gmail.com>
2020-12-28 19:05:01 -0600

    Finishing the styles in the footer component

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -11,22 +11,59 @@ export default function Footer() {
         <div className="Footer__container-card">
           <h2>Quick Links</h2>
           <div>
-            <a href="/#hero">Home</a>
+            <a href="/#hero">Header</a>
             <a href="/#about">About Me</a>
             <a href="/#portfolio">My Portfolio</a>
+            <a
+              href="mailto:lolesuncrak@gmail.com"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Send Me a Email
+            </a>{" "}
           </div>
         </div>
         <div className="Footer__container-card">
           <h2>Social Media</h2>
           <div>
-            <a href="/">LinkedIn</a>
-            <a href="/">Twitter</a>
-            <a href="/">Github</a>
-            <a href="/">Facebook</a>
-            <a href="/">Instagram</a>
+            <a
+              href="https://www.linkedin.com/in/mrLuisFer/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              LinkedIn
+            </a>
+            <a
+              href="https://twitter.com/lolesuncrak"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Twitter
+            </a>
+            <a
+              href="https://github.com/mrLuisFer/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Github
+            </a>
+            <a
+              href="https://www.facebook.com/profile.php?id=100013068189807"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Facebook
+            </a>
+            <a
+              href="https://www.instagram.com/mrluisfer_/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Instagram
+            </a>
           </div>
         </div>
-
+        {/* Footer Illustration */}
         <img
           className="Footer__container-illustration"
           src={footerIllustration}
