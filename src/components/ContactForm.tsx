4a080e01b48ca3887622aa74d0a11eef524cfede
mrLuisFer <lolesuncrak@gmail.com>
2020-12-29 15:06:08 -0600

    FIxing some bugs in the sass fyles

--- src/components/ContactForm.tsx
+++ src/components/ContactForm.tsx
@@ -3,8 +3,12 @@ import React from "react"
 // This is the contact component with all logic
 export default function ContactForm() {
   return (
-    <div>
-      <p>Fom</p>
+    <div className="ContactForm">
+      <h2>Leave me a message</h2>
+      {/* Contact Form */}
+      <form action="" method="post">
+        <input type="text" name="" id="" />
+      </form>
     </div>
   )
 }
