e1f03332fce8300777543cbba2e9d7b9fbab8c30
mrLuisFer <lolesuncrak@gmail.com>
2020-12-29 18:51:26 -0600

    Adding emailJs and the functionality to send emails

--- src/components/ContactForm.tsx
+++ src/components/ContactForm.tsx
@@ -1,4 +1,5 @@
 import React from "react"
+import { sendEmail } from "../hooks/emailJs"
 
 // This is the contact component with all logic
 export default function ContactForm() {
@@ -6,8 +7,11 @@ export default function ContactForm() {
     <div className="ContactForm">
       <h2>Leave me a message</h2>
       {/* Contact Form */}
-      <form action="" method="post">
-        <input type="text" name="" id="" />
+      <form method="post" onSubmit={sendEmail}>
+        <input type="text" name="from_name" />
+        <input type="emal" name="email" />
+        <input type="text" name="message" />
+        <button type="submit">Send Email</button>
       </form>
     </div>
   )
