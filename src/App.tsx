87ed84076e22aa3ab666437b26dff4ce5348d64b
mrLuisFer <lolesuncrak@gmail.com>
2021-04-04 13:57:38 -0500

    fix: files .tsx fixed in eslint

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,29 @@
-import "./scss/_App.scss"
+import { JSX } from 'react'
+import './scss/_App.scss'
 // React Router
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 
 // Assets
-import wavePattern from "./assets/wave-pattern.svg"
+import wavePattern from './assets/wave-pattern.svg'
 // views
-import HomePage from "./views/HomePage"
-import ContactUs from "./views/ContactMe"
+import HomePage from './views/HomePage'
+import ContactUs from './views/ContactMe'
 // components
-import NavBar from "./components/NavBar"
-import Footer from "./components/Footer"
-import BtnToTop from "./components/BtnToTop/BtnToTop"
+import NavBar from './components/NavBar'
+import Footer from './components/Footer'
+import BtnToTop from './components/BtnToTop/BtnToTop'
 
 export default function App(): JSX.Element {
   // Components for the view
   return (
     <Router>
-      <div className="App">
-        <img className="wave-pattern" src={wavePattern} alt="wave-pattern" />
+      <div className='App'>
+        <img className='wave-pattern' src={wavePattern} alt='wave-pattern' />
         <NavBar />
 
         <Switch>
-          <Route exact path="/" component={HomePage}></Route>
-          <Route path="/contact-me" component={ContactUs}></Route>
+          <Route exact path='/' component={HomePage} />
+          <Route path='/contact-me' component={ContactUs} />
         </Switch>
 
         <BtnToTop />
