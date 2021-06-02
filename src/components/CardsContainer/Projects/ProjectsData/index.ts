0c3d6d29649885623d4208897fba8902ef31dc2c
mrLuisFer <lolesuncrak@gmail.com>
2021-06-01 23:50:39 -0500

    feat: projects data basic created

---
+++ src/components/CardsContainer/Projects/ProjectsData/index.ts
@@ -0,0 +1,17 @@
+type TProjectsData = {
+  title: string
+  link: string
+  preview?: string
+  repo: string
+  contributors: Array<string>
+}
+
+export const projectsData: Array<TProjectsData> = [
+  {
+    title: '',
+    link: '',
+    preview: '',
+    repo: '',
+    contributors: ['Luis'],
+  },
+]
