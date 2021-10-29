052e8db9d1924d816a79990ecd4d357354c7be58
mrLuisFer <mrluisfeer@gmail.com>
2021-10-29 08:18:49 -0500

    remove webVitals file

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,6 @@
 import { StrictMode } from 'react'
 import { render } from 'react-dom'
 import App from './app'
-// import reportWebVitals from './reportWebVitals'
 
 import './styles/global/normalize.css'
 import './styles/global/variables.css'
