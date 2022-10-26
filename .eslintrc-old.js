9f70b70e5793f883aec058bb07d20a85239aeb3c
Luis Alvarez <luis.alvarez@nowports.com>
2022-10-25 22:11:49 -0500

    fix eslint problems

---
+++ .eslintrc-old.js
@@ -0,0 +1,116 @@
+// {
+//   "env": {
+//     "browser": true,
+//     "es2021": true
+//   },
+//   "globals": {
+//     "ASSET_PATH": true
+//   },
+//   "extends": [
+//     "plugin:react/recommended",
+//     "airbnb",
+//     "plugin:react/recommended",
+//     "plugin:prettier/recommended"
+//   ],
+//   "parser": "@typescript-eslint/parser",
+//   "parserOptions": {
+//     "ecmaFeatures": {
+//       "jsx": true
+//     },
+//     "project": "./tsconfig.json",
+//     "ecmaVersion": 12,
+//     "sourceType": "module"
+//   },
+//   "plugins": ["react", "@typescript-eslint", "prettier", "react-hooks"],
+//   "rules": {
+//     "react/react-in-jsx-scope": 0,
+//     "eqeqeq": "error",
+//     "no-proto": "warn",
+//     "no-unused-vars": "error",
+//     "no-unused-labels": "warn",
+//     "strict": "warn",
+//     "jsx-quotes": ["warn", "prefer-single"],
+//     "semi": ["warn", "never"],
+//     "quotes": ["warn", "single"],
+//     "no-alert": "warn",
+//     "import/no-extraneous-dependencies": "off",
+//     "import/extensions": "off",
+//     "react/no-multi-comp": 0,
+//     "react/sort-comp": 0,
+//     "react/require-default-props": 0,
+//     "react/default-props-match-prop-types": [
+//       "error",
+//       {
+//         "allowRequiredDefaults": true
+//       }
+//     ],
+//     "import/no-unresolved": "off",
+//     "react/destructuring-assignment": 0,
+//     "jsx-a11y/no-static-element-interactions": 0,
+//     "jsx-a11y/label-has-for": 0,
+//     "jsx-a11y/img-has-alt": 0,
+//     "camelcase": 0, // relay generates types with _ in the names, so need to allow non-camelcased,
+//     "no-underscore-dangle": 0,
+//     "lines-between-class-members": 0,
+//     "no-continue": 0,
+//     "react/static-property-placement": 0,
+//     "react/state-in-constructor": 0,
+//     "react/jsx-props-no-spreading": 0,
+//     "react/jsx-filename-extension": [1, { "extensions": [".js", ".jsx", ".ts", ".tsx"] }],
+//     "no-undef": "off",
+//     "react-hooks/rules-of-hooks": "error", // Checks rules of Hooks
+//     "react-hooks/exhaustive-deps": "warn",
+//     "no-else-return": "off",
+//     "import/prefer-default-export": "off",
+//     "no-param-reassign": "off",
+//     "operator-linebreak": "off",
+//     "implicit-arrow-linebreak": "off",
+//     "max-len": "off",
+//     "indent": "off",
+//     "no-shadow": "off",
+//     "arrow-parens": "off",
+//     "no-confusing-arrow": "off",
+//     "no-use-before-define": "off",
+//     "object-curly-newline": "off",
+//     "function-paren-newline": "off",
+//     "no-restricted-globals": "off",
+//     "jsx-a11y/anchor-is-valid": "off",
+//     "react/function-component-definition": "off"
+//   },
+//   "settings": {
+//     "import/resolver": {
+//       "node": {
+//         "extensions": [".js", ".jsx", ".ts", ".tsx"]
+//       }
+//     },
+//     "react": {
+//       "createClass": "createReactClass", // Regex for Component Factory to use,
+//       // default to "createReactClass"
+//       "pragma": "React", // Pragma to use, default to "React"
+//       "fragment": "Fragment", // Fragment to use (may be a property of <pragma>), default to "Fragment"
+//       "version": "detect", // React version. "detect" automatically picks the version you have installed.
+//       // You can also use `16.0`, `16.3`, etc, if you want to override the detected value.
+//       // default to latest and warns if missing
+//       // It will default to "detect" in the future
+//       "flowVersion": "0.53" // Flow version
+//     },
+//     "propWrapperFunctions": [
+//       // The names of any function used to wrap propTypes, e.g. `forbidExtraProps`. If this isn't set, any propTypes wrapped in a function will be skipped.
+//       "forbidExtraProps",
+//       { "property": "freeze", "object": "Object" },
+//       { "property": "myFavoriteWrapper" }
+//     ],
+//     "componentWrapperFunctions": [
+//       // The name of any function used to wrap components, e.g. Mobx `observer` function. If this isn't set, components wrapped by these functions will be skipped.
+//       "observer", // `property`
+//       { "property": "styled" }, // `object` is optional
+//       { "property": "observer", "object": "Mobx" },
+//       { "property": "observer", "object": "<pragma>" } // sets `object` to whatever value `settings.react.pragma` is set to
+//     ],
+//     "linkComponents": [
+//       // Components used as alternatives to <a> for linking, eg. <Link to={ url } />
+//       "Hyperlink",
+//       { "name": "Link", "linkAttribute": "to" }
+//     ]
+//   }
+// }
