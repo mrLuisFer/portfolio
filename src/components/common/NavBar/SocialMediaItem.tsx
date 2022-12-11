6f670a2470ae7891a61fd5244ec2a5f09949333d
Luis Alvarez <luis.alvarez@nowports.com>
2022-12-10 23:55:27 -0600

    fix state for navbar icon

--- src/components/common/NavBar/SocialMediaItem.tsx
+++ src/components/common/NavBar/SocialMediaItem.tsx
@@ -1,6 +1,7 @@
-import { Box, useDisclosure, SlideFade } from '@chakra-ui/react'
+import { Box, SlideFade } from '@chakra-ui/react'
 import Link from '../custom/Link'
 import { TbExternalLink } from 'react-icons/tb'
+import { useState } from 'react'
 
 interface ISocialMediaItemProps {
   href: string
@@ -9,26 +10,26 @@ interface ISocialMediaItemProps {
 }
 
 export default function SocialMediaItem({ item }: { item: ISocialMediaItemProps }) {
-  const { isOpen, onToggle } = useDisclosure()
+  const [showIcon, setShowIcon] = useState<boolean>(false)
 
   return (
     <Box
       key={item.label}
-      onMouseEnter={onToggle}
-      onMouseLeave={onToggle}
+      onMouseEnter={() => setShowIcon(true)}
+      onMouseLeave={() => setShowIcon(false)}
       display='flex'
       alignItems='center'
       gap='5px'>
       <Link
         href={item.href}
         external={item.external}
-        opacity='0.5'
+        opacity={showIcon ? '1' : '0.5'}
         _hover={{ opacity: '1' }}>
         {item.label}
       </Link>
       {item.external && (
-        <SlideFade in={isOpen} offsetX='20px' offsetY='0'>
-          <Box>
+        <SlideFade in={showIcon} offsetX='20px' offsetY='0'>
+          <Box cursor='pointer'>
             <TbExternalLink />
           </Box>
         </SlideFade>
