4049b0559978af1962ac188f7bbcd10a26083374
Luis Alvarez <luis.alvarez@nowports.com>
2022-12-10 23:30:39 -0600

    add analytics object

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -57,13 +57,21 @@ export default function AppPage({ Component, pageProps }: AppProps) {
         </Script>
         <Script>
           {`
-          <!-- Google Tag Manager -->
           <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
           new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
           j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-        })(window,document,'script','dataLayer','GTM-PBHNMXT');</script>
-        <!-- End Google Tag Manager -->
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})
+          (window,document,'script','dataLayer','GTM-PBHNMXT');</script>
+          `}
+        </Script>
+        <Script>
+          {`
+          (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+            (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+            m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+          })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+          ga('create', ${process.env.ANALYTICS}, 'auto');  // Creates a tracker.
+          ga('send', 'pageview');             // Sends a pageview.
           `}
         </Script>
       </Head>
