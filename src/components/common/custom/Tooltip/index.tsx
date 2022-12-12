bff2d71f349b451f3b896f8ad4bf3314f762c467
Luis Alvarez <65029792+mrLuisFer@users.noreply.github.com>
2022-12-12 00:27:36 -0600

    update ui experience for drag and drop section (#21)

--- src/components/common/custom/Tooltip/index.tsx
+++ src/components/common/custom/Tooltip/index.tsx
@@ -1,8 +1,22 @@
-import { Box, Tooltip as ChakraTooltip } from '@chakra-ui/react'
+import { Box, PlacementWithLogical, Tooltip as ChakraTooltip } from '@chakra-ui/react'
 
-export default function Tooltip({ label, children }: { label: string; children: any }) {
+export default function Tooltip({
+  label,
+  children,
+  placement = 'bottom',
+}: {
+  label: string
+  children: any
+  placement?: PlacementWithLogical
+}) {
   return (
-    <ChakraTooltip label={label} hasArrow color='white' borderRadius='10px' bg='#222'>
+    <ChakraTooltip
+      label={label}
+      hasArrow
+      color='white'
+      borderRadius='10px'
+      bg='#222'
+      placement={placement}>
       <Box position='relative'>{children}</Box>
     </ChakraTooltip>
   )
--- src/components/pages/home/TechSkills/TechItem.tsx
+++ src/components/pages/home/TechSkills/TechItem.tsx
@@ -21,7 +21,11 @@ export default function TechItem({ item }: { item: TechIcons }) {
         fontSize={['30px', '50px']}
         opacity='0.8'
         transition='0.1s ease'
-        _hover={{ opacity: '1', color: item.color }}>
+        _hover={{ opacity: '1', color: item.color }}
+        cursor='grab'
+        _active={{
+          cursor: 'grabbing',
+        }}>
         <Tooltip label={item.label}>
           <Box
             display={'flex'}
