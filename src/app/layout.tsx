957665502a96dbf5fcec1bc9d246244e5ff3ec41
Luis Alvarez <mrluisfeer@gmail.com>
2024-11-22 00:35:47 -0600

    remove or operator

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,7 +7,7 @@ import { TooltipProvider } from '@/components/ui/tooltip'
 import { Analytics } from '@vercel/analytics/react'
 import { useChristmasDate } from '@/hooks/useChristmasDate'
 
-const isOnDevEnv = process?.env?.NODE_ENV === 'development' || true
+const isOnDevEnv = process?.env?.NODE_ENV === 'development'
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   const isChristmas = useChristmasDate()
 
